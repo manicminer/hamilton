52b12119fadcd13f1b67158604e1184175bba604
Tom Bamford <tom@bamford.io>
2020-08-26 01:48:11 +0100

    Get a single domain

--- clients/domains.go
+++ clients/domains.go
@@ -3,11 +3,13 @@ package clients
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/manicminer/hamilton/auth"
 	"github.com/manicminer/hamilton/base"
 	"github.com/manicminer/hamilton/models"
-	"io/ioutil"
-	"net/http"
 )
 
 type DomainsClient struct {
@@ -39,4 +41,19 @@ func (c *DomainsClient) List(ctx context.Context) (*[]models.Domain, error) {
 	return &data.Domains, nil
 }
 
-
+func (c *DomainsClient) Get(ctx context.Context, id string) (*models.Domain, error) {
+	resp, err := c.BaseClient.Get(ctx, base.GetHttpRequestInput{
+		ValidStatusCodes: []int{http.StatusOK},
+		Uri:              fmt.Sprintf("/domains/%s", id),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	var domain models.Domain
+	if err := json.Unmarshal(respBody, &domain); err != nil {
+		return nil, err
+	}
+	return &domain, nil
+}
