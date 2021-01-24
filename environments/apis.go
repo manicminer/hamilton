fa41ce668ec32a1e4f48cf24858fa8e2ed4a1180
Tom Bamford <tom@bamford.io>
2021-01-24 19:08:16 +0000

    Export known API app IDs as map for easier consumption

--- environments/apis.go
+++ environments/apis.go
@@ -1,106 +1,5 @@
 package environments
 
-type ApiAppId string
-
-const (
-	ApplicationInsights               ApiAppId = "f5c26e74-f226-4ae8-85f0-b4af0080ac9e"
-	AttestationService                ApiAppId = "c61423b7-1d1f-430d-b444-0eee53298103"
-	AzureActiveDirectoryGraph         ApiAppId = "00000002-0000-0000-c000-000000000000"
-	AzureAdIdentityGovernanceInsights ApiAppId = "58c746b0-a0b0-4647-a8f6-12dde5981638"
-	AzureAdIntegratedApp              ApiAppId = "af47b99c-8954-4b45-ab68-8121157418ef"
-	AzureAdNotification               ApiAppId = "fc03f97a-9db0-4627-a216-ec98ce54e018"
-	AzureAnalysisServices             ApiAppId = "4ac7d521-0382-477b-b0f8-7e1d95f85ca2"
-	AzureAppConfiguration             ApiAppId = "35ffadb3-7fc1-497e-b61b-381d28e744cc"
-	AzureAppService                   ApiAppId = "abfa0a7c-a6b6-4736-8310-5855508787cd"
-	AzureBatch                        ApiAppId = "ddbf3205-c6bd-46ae-8127-60eb93363864"
-	AzureContainerRegistry            ApiAppId = "6a0ec4d3-30cb-4a83-91c0-ae56bc0e3d26"
-	AzureCosmosDb                     ApiAppId = "a232010e-820c-4083-83bb-3ace5fc29d0b"
-	AzureDataBricks                   ApiAppId = "2ff814a6-3304-4ab8-85cb-cd0e6f879c1d"
-	AzureDataCatalog                  ApiAppId = "9d3e55ba-79e0-4b7c-af50-dc460b81dca1"
-	AzureDataLake                     ApiAppId = "e9f49c6b-5ce5-44c8-925d-015017e9f7ad"
-	AzureDevOps                       ApiAppId = "499b84ac-1321-427f-aa17-267ca6975798"
-	AzureDigitalTwins                 ApiAppId = "0b07f429-9f4b-4714-9392-cc5e8e80c8b0"
-	AzureEventHubs                    ApiAppId = "80369ed6-5f11-4dd9-bef3-692475845e77"
-	AzureHdInsightCluster             ApiAppId = "7865c1d2-f040-46cc-875f-831a1ef6a28a"
-	AzureHealthcare                   ApiAppId = "4f6778d8-5aef-43dc-a1ff-b073724b9495"
-	AzureIamSupportability            ApiAppId = "a57aca87-cbc0-4f3c-8b9e-dc095fdc8978"
-	AzureImportExport                 ApiAppId = "7de4d5c5-5b32-4235-b8a9-33b34d6bcd2a"
-	AzureIotCentral                   ApiAppId = "9edfcdd9-0bc5-4bd4-b287-c3afc716aac7"
-	AzureIotHubDeviceProvisioning     ApiAppId = "0cd79364-7a90-4354-9984-6e36c841418d"
-	AzureKeyVault                     ApiAppId = "cfa8b339-82a2-471a-a3c9-0fc0be7a4093"
-	AzureKubernetesServiceAadServer   ApiAppId = "6dae42f8-4368-4678-94ff-3960e28e3630"
-	AzureMaps                         ApiAppId = "ba1ea022-5807-41d5-bbeb-292c7e1cf5f6"
-	AzureMediaServices                ApiAppId = "374b2a64-3b6b-436b-934c-b820eacca870"
-	AzureServiceBus                   ApiAppId = "80a10ef9-8168-493d-abf9-3297c4ef6e3c"
-	AzureServiceDeploy                ApiAppId = "5b306cba-9c71-49db-96c3-d17ca2379c4d"
-	AzureServiceManagement            ApiAppId = "797f4846-ba00-4fd7-ba43-dac1f8f63013"
-	AzureSqlDatabase                  ApiAppId = "022907d3-0f1b-48f7-badc-1ba6abab6d66"
-	AzureStackHciService              ApiAppId = "1322e676-dee7-41ee-a874-ac923822781c"
-	AzureStorage                      ApiAppId = "e406a681-f3d4-42a8-90b6-c2b029497af1"
-	AzureStreamAnalytics              ApiAppId = "66f1e791-7bfb-4e18-aed8-1720056421c7"
-	AzureSynapseGateway               ApiAppId = "1ac05c7e-12d2-4605-bf9d-549d7041c6b3"
-	AzureSynapseStudio                ApiAppId = "ec52d13d-2e85-410e-a89a-8c79fb6a32ac"
-	AzureTimeSeriesInsights           ApiAppId = "120d688d-1518-4cf7-bd38-182f158850b6"
-	Bing                              ApiAppId = "9ea1ad79-fdb6-4f9a-8bc3-2b70f96e34c7"
-	BotFrameworkDevPortal             ApiAppId = "f3723d34-6ff5-4ceb-a148-d99dcd2511fc"
-	BranchConnectWebService           ApiAppId = "57084ef3-d413-4087-a28f-f6f3b1ad7786"
-	CognitiveServices                 ApiAppId = "7d312290-28c8-473c-a0ed-8e53749b6d6d"
-	ComputeRecommendationService      ApiAppId = "b9a92e36-2cf8-4f4e-bcb3-9d99e00e14ab"
-	ConnectionsService                ApiAppId = "b7912db9-aa33-4820-9d4f-709830fdd78f"
-	CortanaAtWorkBingServices         ApiAppId = "22d7579f-06c2-4baa-89d2-e844486adb9d"
-	CortanaAtWorkService              ApiAppId = "2a486b53-dbd2-49c0-a2bc-278bdfc30833"
-	CortanaRuntimeService             ApiAppId = "81473081-50b9-469a-b9d8-303109583ecb"
-	CustomerInsights                  ApiAppId = "38c77d00-5fcb-4cce-9d93-af4738258e3c"
-	DataMigrationService              ApiAppId = "a4bad4aa-bf02-4631-9f78-a64ffdba8150"
-	Dynamic365BusinessCentral         ApiAppId = "996def3d-b36c-4153-8607-a6fd3c01b89f"
-	Dynamics365DataExportService      ApiAppId = "b861dbcc-a7ef-4219-a005-0e4de4ea7dcf"
-	DynamicsCrm                       ApiAppId = "00000007-0000-0000-c000-000000000000"
-	DynamicsErp                       ApiAppId = "00000015-0000-0000-c000-000000000000"
-	FlowService                       ApiAppId = "7df0a125-d3be-4c96-aa54-591f83ff541c"
-	GraphConnectorService             ApiAppId = "56c1da01-2129-48f7-9355-af6d59d42766"
-	InformationProtectionSyncService  ApiAppId = "870c4f2e-85b6-4d43-bdda-6ed9a579b725"
-	InTune                            ApiAppId = "c161e42e-d4df-4a3d-9b42-e7a3c31f59d4"
-	KustoService                      ApiAppId = "2746ea77-4702-4b45-80ca-3c97e680e8b7"
-	KustoServiceMFA                   ApiAppId = "725d0e77-e1fd-48f1-a295-2115457f7609"
-	LogAnalytics                      ApiAppId = "ca7f3f0b-7d91-482c-8e09-c5d840d0eac5"
-	MileIqAdminCenter                 ApiAppId = "de096ee1-dae7-4ee1-8dd5-d88ccc473815"
-	MileIqDashboard                   ApiAppId = "f7069a8d-9edc-4300-b365-ae53c9627fc4"
-	MileIqRestService                 ApiAppId = "b692184e-b47f-4706-b352-84b288d2d9ee"
-	MixedReality                      ApiAppId = "c7ddd9b4-5172-4e28-bd29-1e0792947d18"
-	MicrosoftAzureCli                 ApiAppId = "04b07795-8ddb-461a-bbee-02f9e1bf7b46"
-	Microsoft365DataAtRestEncryption  ApiAppId = "c066d759-24ae-40e7-a56f-027002b5d3e4"
-	MicrosoftGraph                    ApiAppId = "00000003-0000-0000-c000-000000000000"
-	MicrosoftInvoicing                ApiAppId = "b6b84568-6c01-4981-a80f-09da9a20bbed"
-	Office365Connectors               ApiAppId = "48af08dc-f6d2-435f-b2a7-069abd99c086"
-	Office365Demeter                  ApiAppId = "982bda36-4632-4165-a46a-9863b1bbcf7d"
-	Office365DwEngineV2               ApiAppId = "441509e5-a165-4363-8ee7-bcf0b7d26739"
-	Office365ExchangeOnline           ApiAppId = "00000002-0000-0ff1-ce00-000000000000"
-	Office365ExchangeOnlineProtection ApiAppId = "00000007-0000-0ff1-ce00-000000000000"
-	Office365InformationProtection    ApiAppId = "2f3f02c9-5679-4a5c-a605-0de55b07d135"
-	Office365Management               ApiAppId = "c5393580-f805-4401-95e8-94b7a6ef2fc2"
-	Office365SharePointOnline         ApiAppId = "00000003-0000-0ff1-ce00-000000000000"
-	Office365Zoom                     ApiAppId = "0d38933a-0bbd-41ca-9ebd-28c4b5ba7cb7"
-	OneNote                           ApiAppId = "2d4d3d8e-2be3-4bef-9f87-7875a61c29de"
-	OneProfileService                 ApiAppId = "b2cc270f-563e-4d8a-af47-f00963a71dcd"
-	OssRdbms                          ApiAppId = "123cd850-d9df-40bd-94d5-c9f07b7fa203"
-	PeopleCardsService                ApiAppId = "394866fc-eedb-4f01-8536-3ff84b16be2a"
-	PolicyAdministrationService       ApiAppId = "0469d4cd-df37-4d93-8a61-f8c75b809164"
-	PowerAppsRuntimeService           ApiAppId = "82f77645-8a66-4745-bcdf-9706824f9ad0"
-	PowerBiService                    ApiAppId = "00000009-0000-0000-c000-000000000000"
-	Purview                           ApiAppId = "73c2949e-da2d-457a-9607-fcc665198967"
-	RightsManagementServices          ApiAppId = "00000012-0000-0000-c000-000000000000"
-	ServiceTrust                      ApiAppId = "d6fdaa33-e821-4211-83d0-cf74736489e1"
-	Signup                            ApiAppId = "b4bddae8-ab25-483e-8670-df09b9f1d0ea"
-	SkypeForBusinessOnline            ApiAppId = "00000004-0000-0ff1-ce00-000000000000"
-	SpeechRecognition                 ApiAppId = "1a6fcee6-0816-469b-acac-fe7ef2e87b83"
-	TargetedMessagingService          ApiAppId = "4c4f550b-42b2-4a16-93f9-fdb9e01bb6ed"
-	ThreatProtection                  ApiAppId = "8ee8fdad-f234-4243-8f3b-15c294843740"
-	UniversalPrint                    ApiAppId = "da9b70f6-5323-4ce6-ae5c-88dcc5082966"
-	WindowsDefenderAtp                ApiAppId = "fc780465-2017-40d4-a0c5-307022471b92"
-	WindowsVirtualDesktop             ApiAppId = "9cdead84-a844-4324-93f2-b2e6bb768d07"
-	Yammer                            ApiAppId = "00000005-0000-0ff1-ce00-000000000000"
-)
-
 type ApiEndpoint string
 
 const (
@@ -131,61 +30,61 @@ type Api struct {
 
 var (
 	MsGraphGlobal = Api{
-		AppId:    MicrosoftGraph,
+		AppId:    PublishedApis["MicrosoftGraph"],
 		CliName:  MsGraphCliName,
 		Endpoint: MsGraphGlobalEndpoint,
 	}
 
 	MsGraphGermany = Api{
-		AppId:    MicrosoftGraph,
+		AppId:    PublishedApis["MicrosoftGraph"],
 		CliName:  MsGraphCliName,
 		Endpoint: MsGraphGermanyEndpoint,
 	}
 
 	MsGraphChina = Api{
-		AppId:    MicrosoftGraph,
+		AppId:    PublishedApis["MicrosoftGraph"],
 		CliName:  MsGraphCliName,
 		Endpoint: MsGraphChinaEndpoint,
 	}
 
 	MsGraphUSGovL4 = Api{
-		AppId:    MicrosoftGraph,
+		AppId:    PublishedApis["MicrosoftGraph"],
 		CliName:  MsGraphCliName,
 		Endpoint: MsGraphUSGovL4Endpoint,
 	}
 
 	MsGraphUSGovL5 = Api{
-		AppId:    MicrosoftGraph,
+		AppId:    PublishedApis["MicrosoftGraph"],
 		CliName:  MsGraphCliName,
 		Endpoint: MsGraphUSGovL5Endpoint,
 	}
 
 	MsGraphCanary = Api{
-		AppId:    MicrosoftGraph,
+		AppId:    PublishedApis["MicrosoftGraph"],
 		CliName:  MsGraphCliName,
 		Endpoint: MsGraphCanaryEndpoint,
 	}
 
 	AadGraphGlobal = Api{
-		AppId:    AzureActiveDirectoryGraph,
+		AppId:    PublishedApis["AzureActiveDirectoryGraph"],
 		CliName:  AadGraphCliName,
 		Endpoint: AadGraphGlobalEndpoint,
 	}
 
 	AadGraphGermany = Api{
-		AppId:    AzureActiveDirectoryGraph,
+		AppId:    PublishedApis["AzureActiveDirectoryGraph"],
 		CliName:  AadGraphCliName,
 		Endpoint: AadGraphGermanyEndpoint,
 	}
 
 	AadGraphChina = Api{
-		AppId:    AzureActiveDirectoryGraph,
+		AppId:    PublishedApis["AzureActiveDirectoryGraph"],
 		CliName:  AadGraphCliName,
 		Endpoint: AadGraphChinaEndpoint,
 	}
 
 	AadGraphUSGov = Api{
-		AppId:    AzureActiveDirectoryGraph,
+		AppId:    PublishedApis["AzureActiveDirectoryGraph"],
 		CliName:  AadGraphCliName,
 		Endpoint: AadGraphUSGovEndpoint,
 	}
