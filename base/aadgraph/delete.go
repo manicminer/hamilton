a2126ef865d6f4db55d6e251931363328802cd7b
Tom Bamford <tom@bamford.io>
2021-01-24 17:25:17 +0000

    AAD Graph base client

---
+++ base/aadgraph/delete.go
@@ -0,0 +1,42 @@
+package aadgraph
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+// DeleteHttpRequestInput configures a DELETE request.
+type DeleteHttpRequestInput struct {
+	ValidStatusCodes []int
+	ValidStatusFunc  ValidStatusFunc
+	Uri              Uri
+}
+
+// GetValidStatusCodes returns a []int of status codes considered valid for a DELETE request.
+func (i DeleteHttpRequestInput) GetValidStatusCodes() []int {
+	return i.ValidStatusCodes
+}
+
+// GetValidStatusFunc returns a function used to evaluate whether the response to a DELETE request is considered valid.
+func (i DeleteHttpRequestInput) GetValidStatusFunc() ValidStatusFunc {
+	return i.ValidStatusFunc
+}
+
+// Delete performs a DELETE request.
+func (c Client) Delete(ctx context.Context, input DeleteHttpRequestInput) (*http.Response, int, error) {
+	var status int
+	url, err := c.buildUri(input.Uri)
+	if err != nil {
+		return nil, status, fmt.Errorf("unable to make request: %v", err)
+	}
+	req, err := http.NewRequestWithContext(ctx, http.MethodDelete, url, http.NoBody)
+	if err != nil {
+		return nil, status, err
+	}
+	resp, status, _, err := c.performRequest(req, input)
+	if err != nil {
+		return nil, status, err
+	}
+	return resp, status, nil
+}
