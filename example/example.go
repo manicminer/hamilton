93f2f905c307f3006bea04826e9a0e4fa91fb30f
Tom Bamford <tom@bamford.io>
2021-01-27 23:45:40 +0000

    Commit buildable example

---
+++ example/example.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/manicminer/hamilton/auth"
+	"github.com/manicminer/hamilton/clients"
+	"github.com/manicminer/hamilton/environments"
+)
+
+var (
+	tenantId           = os.Getenv("TENANT_ID")
+	clientId           = os.Getenv("CLIENT_ID")
+	clientSecret       = os.Getenv("CLIENT_SECRET")
+)
+
+func main() {
+	ctx := context.Background()
+
+	authConfig := &auth.Config{
+		Environment:            environments.Global,
+		TenantID:               tenantId,
+		ClientID:               clientId,
+		ClientSecret:           clientSecret,
+		EnableClientSecretAuth: true,
+	}
+
+	authorizer, err := authConfig.NewAuthorizer(ctx, auth.MsGraph)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := clients.NewUsersClient(tenantId)
+	client.BaseClient.Authorizer = authorizer
+
+	users, _, err := client.List(ctx, "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if users == nil {
+		log.Fatalln("bad API response, nil result received")
+	}
+
+	for _, user := range *users {
+		fmt.Printf("%s: %s <%s>\n", *user.ID, *user.DisplayName, *user.UserPrincipalName)
+	}
+}
