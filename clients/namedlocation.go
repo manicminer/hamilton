72fd0874432d765f804269c8ff4b3262f0110af8
Alex Wilcox <alex.wilcox@cloudsecure.ltd>
2021-04-07 23:02:58 +0100

    refactor to create common functions

---
+++ clients/namedlocation.go
@@ -0,0 +1,219 @@
+package clients
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/manicminer/hamilton/base"
+	"github.com/manicminer/hamilton/internal/utils"
+	"github.com/manicminer/hamilton/models"
+)
+
+// NamedLocationClient performs operations on Named Locations.
+type NamedLocationClient struct {
+	BaseClient base.Client
+}
+
+// NewNamedLocationClient returns a new NamedLocationClient.
+func NewNamedLocationClient(tenantId string) *NamedLocationClient {
+	return &NamedLocationClient{
+		BaseClient: base.NewClient(base.Version10, tenantId),
+	}
+}
+
+// List returns a list of Named Locations, optionally filtered using OData.
+func (c *NamedLocationClient) List(ctx context.Context, filter string) (*[]models.NamedLocation, int, error) {
+	params := url.Values{}
+	if filter != "" {
+		params.Add("$filter", filter)
+	}
+
+	resp, status, _, err := c.BaseClient.Get(ctx, base.GetHttpRequestInput{
+		ValidStatusCodes: []int{http.StatusOK},
+		Uri: base.Uri{
+			Entity:      "/identity/conditionalAccess/namedLocations",
+			Params:      params,
+			HasTenantId: true,
+		},
+	})
+
+	if err != nil {
+		return nil, status, err
+	}
+
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	var data struct {
+		NamedLocations []models.NamedLocation `json:"value"`
+	}
+
+	if err := json.Unmarshal(respBody, &data); err != nil {
+		return nil, status, err
+	}
+
+	return &data.NamedLocations, status, nil
+}
+
+// Delete removes a Named Location.
+func (c *NamedLocationClient) Delete(ctx context.Context, id string) (int, error) {
+	_, status, _, err := c.BaseClient.Delete(ctx, base.DeleteHttpRequestInput{
+		ValidStatusCodes: []int{http.StatusNoContent},
+		Uri: base.Uri{
+			Entity:      fmt.Sprintf("/identity/conditionalAccess/namedLocations/%s", id),
+			HasTenantId: true,
+		},
+	})
+	if err != nil {
+		return status, err
+	}
+	return status, nil
+}
+
+// CreateIP creates a new IP Named Location.
+func (c *NamedLocationClient) CreateIP(ctx context.Context, ipNamedLocation models.IPNamedLocation) (*models.IPNamedLocation, int, error) {
+	var status int
+
+	ipNamedLocation.ODataType = utils.StringPtr("#microsoft.graph.ipNamedLocation")
+
+	body, err := json.Marshal(ipNamedLocation)
+	if err != nil {
+		return nil, status, err
+	}
+	resp, status, _, err := c.BaseClient.Post(ctx, base.PostHttpRequestInput{
+		Body:             body,
+		ValidStatusCodes: []int{http.StatusCreated},
+		Uri: base.Uri{
+			Entity:      "/identity/conditionalAccess/namedLocations",
+			HasTenantId: true,
+		},
+	})
+	if err != nil {
+		return nil, status, err
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	var newIPNamedLocation models.IPNamedLocation
+	if err := json.Unmarshal(respBody, &newIPNamedLocation); err != nil {
+		return nil, status, err
+	}
+	return &newIPNamedLocation, status, nil
+}
+
+// CreateCountry creates a new Country Named Location.
+func (c *NamedLocationClient) CreateCountry(ctx context.Context, countryNamedLocation models.CountryNamedLocation) (*models.CountryNamedLocation, int, error) {
+	var status int
+
+	countryNamedLocation.ODataType = utils.StringPtr("#microsoft.graph.countryNamedLocation")
+
+	body, err := json.Marshal(countryNamedLocation)
+	if err != nil {
+		return nil, status, err
+	}
+	resp, status, _, err := c.BaseClient.Post(ctx, base.PostHttpRequestInput{
+		Body:             body,
+		ValidStatusCodes: []int{http.StatusCreated},
+		Uri: base.Uri{
+			Entity:      "/identity/conditionalAccess/namedLocations",
+			HasTenantId: true,
+		},
+	})
+	if err != nil {
+		return nil, status, err
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	var newCountryNamedLocation models.CountryNamedLocation
+	if err := json.Unmarshal(respBody, &newCountryNamedLocation); err != nil {
+		return nil, status, err
+	}
+	return &newCountryNamedLocation, status, nil
+}
+
+// GetIP retrieves an IP Named Location.
+func (c *NamedLocationClient) GetIP(ctx context.Context, id string) (*models.IPNamedLocation, int, error) {
+	resp, status, _, err := c.BaseClient.Get(ctx, base.GetHttpRequestInput{
+		ValidStatusCodes: []int{http.StatusOK},
+		Uri: base.Uri{
+			Entity:      fmt.Sprintf("/identity/conditionalAccess/namedLocations/%s", id),
+			HasTenantId: true,
+		},
+	})
+	if err != nil {
+		return nil, status, err
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	var ipNamedLocation models.IPNamedLocation
+	if err := json.Unmarshal(respBody, &ipNamedLocation); err != nil {
+		return nil, status, err
+	}
+	return &ipNamedLocation, status, nil
+}
+
+// GetCountry retrieves an Country Named Location.
+func (c *NamedLocationClient) GetCountry(ctx context.Context, id string) (*models.CountryNamedLocation, int, error) {
+	resp, status, _, err := c.BaseClient.Get(ctx, base.GetHttpRequestInput{
+		ValidStatusCodes: []int{http.StatusOK},
+		Uri: base.Uri{
+			Entity:      fmt.Sprintf("/identity/conditionalAccess/namedLocations/%s", id),
+			HasTenantId: true,
+		},
+	})
+	if err != nil {
+		return nil, status, err
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	var countryNamedLocation models.CountryNamedLocation
+	if err := json.Unmarshal(respBody, &countryNamedLocation); err != nil {
+		return nil, status, err
+	}
+	return &countryNamedLocation, status, nil
+}
+
+// UpdateIP amends an existing IP Named Location.
+func (c *NamedLocationClient) UpdateIP(ctx context.Context, ipNamedLocation models.IPNamedLocation) (int, error) {
+	var status int
+	body, err := json.Marshal(ipNamedLocation)
+	if err != nil {
+		return status, err
+	}
+	_, status, _, err = c.BaseClient.Patch(ctx, base.PatchHttpRequestInput{
+		Body:             body,
+		ValidStatusCodes: []int{http.StatusNoContent},
+		Uri: base.Uri{
+			Entity:      fmt.Sprintf("/identity/conditionalAccess/namedLocations/%s", *ipNamedLocation.ID),
+			HasTenantId: true,
+		},
+	})
+	if err != nil {
+		return status, err
+	}
+	return status, nil
+}
+
+// UpdateCountry amends an existing Country Named Location.
+func (c *NamedLocationClient) UpdateCountry(ctx context.Context, countryNamedLocation models.CountryNamedLocation) (int, error) {
+	var status int
+	body, err := json.Marshal(countryNamedLocation)
+	if err != nil {
+		return status, err
+	}
+	_, status, _, err = c.BaseClient.Patch(ctx, base.PatchHttpRequestInput{
+		Body:             body,
+		ValidStatusCodes: []int{http.StatusNoContent},
+		Uri: base.Uri{
+			Entity:      fmt.Sprintf("/identity/conditionalAccess/namedLocations/%s", *countryNamedLocation.ID),
+			HasTenantId: true,
+		},
+	})
+	if err != nil {
+		return status, err
+	}
+	return status, nil
+}
---
+++ clients/namedlocation_test.go
@@ -0,0 +1,170 @@
+package clients_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/manicminer/hamilton/auth"
+	"github.com/manicminer/hamilton/clients"
+	"github.com/manicminer/hamilton/clients/internal"
+	"github.com/manicminer/hamilton/internal/utils"
+	"github.com/manicminer/hamilton/models"
+)
+
+type NamedLocationClientTest struct {
+	connection   *internal.Connection
+	client       *clients.NamedLocationClient
+	randomString string
+}
+
+func TestNamedLocationClient(t *testing.T) {
+	rs := internal.RandomString()
+	c := NamedLocationClientTest{
+		connection:   internal.NewConnection(auth.MsGraph, auth.TokenVersion2),
+		randomString: rs,
+	}
+	c.client = clients.NewNamedLocationClient(c.connection.AuthConfig.TenantID)
+	c.client.BaseClient.Authorizer = c.connection.Authorizer
+
+	newIPNamedLocation := models.IPNamedLocation{
+		NamedLocation: &models.NamedLocation{
+			DisplayName: utils.StringPtr("Test IP Named Location")},
+		IPRanges: &[]models.IPNamedLocationIPRange{
+			{
+				CIDRAddress: utils.StringPtr("8.8.8.8/32"),
+			},
+			{
+				CIDRAddress: utils.StringPtr("2001:4860:4860::8888/128"),
+			},
+		},
+		IsTrusted: utils.BoolPtr(true),
+	}
+
+	newCountryNamedLocation := models.CountryNamedLocation{
+		NamedLocation: &models.NamedLocation{
+			DisplayName: utils.StringPtr("Test Country Named Location")},
+		CountriesAndRegions: &[]string{"US", "GB"},
+	}
+
+	ipNamedLocation := testNamedLocationClient_CreateIP(t, c, newIPNamedLocation)
+	countryNamedLocation := testNamedLocationClient_CreateCountry(t, c, newCountryNamedLocation)
+
+	// Running get too quickly after create often results in the resource not being found
+	time.Sleep(10 * time.Second)
+	testNamedLocationClient_GetIP(t, c, *ipNamedLocation.ID)
+	testNamedLocationClient_GetCountry(t, c, *countryNamedLocation.ID)
+
+	ipNamedLocation.DisplayName = utils.StringPtr(fmt.Sprintf("test-updated-ipnl-%s", c.randomString))
+	countryNamedLocation.DisplayName = utils.StringPtr(fmt.Sprintf("test-updated-cnl-%s", c.randomString))
+
+	testNamedLocationClient_UpdateIP(t, c, *ipNamedLocation)
+	testNamedLocationClient_UpdateCountry(t, c, *countryNamedLocation)
+
+	testNamedLocationClient_List(t, c, "")
+	testNamedLocationClient_Delete(t, c, *ipNamedLocation.ID)
+	testNamedLocationClient_Delete(t, c, *countryNamedLocation.ID)
+}
+
+func testNamedLocationClient_CreateIP(t *testing.T, c NamedLocationClientTest, ipnl models.IPNamedLocation) (ipNamedLocation *models.IPNamedLocation) {
+	ipNamedLocation, status, err := c.client.CreateIP(c.connection.Context, ipnl)
+	if err != nil {
+		t.Fatalf("NamedLocationClient.CreateIP(): %v", err)
+	}
+	if status < 200 || status >= 300 {
+		t.Fatalf("NamedLocationClient.CreateIP(): invalid status: %d", status)
+	}
+	if ipNamedLocation == nil {
+		t.Fatal("NamedLocationClient.CreateIP(): ipNamedLocation was nil")
+	}
+	if ipNamedLocation.ID == nil {
+		t.Fatal("NamedLocationClient.CreateIP(): ipNamedLocation.ID was nil")
+	}
+	return
+}
+
+func testNamedLocationClient_CreateCountry(t *testing.T, c NamedLocationClientTest, cnl models.CountryNamedLocation) (countryNamedLocation *models.CountryNamedLocation) {
+	countryNamedLocation, status, err := c.client.CreateCountry(c.connection.Context, cnl)
+	if err != nil {
+		t.Fatalf("NamedLocationClient.CreateCountry(): %v", err)
+	}
+	if status < 200 || status >= 300 {
+		t.Fatalf("NamedLocationClient.CreateCountry(): invalid status: %d", status)
+	}
+	if countryNamedLocation == nil {
+		t.Fatal("NamedLocationClient.CreateCountry(): countryNamedLocation was nil")
+	}
+	if countryNamedLocation.ID == nil {
+		t.Fatal("NamedLocationClient.CreateCountry(): countryNamedLocation.ID was nil")
+	}
+	return
+}
+
+func testNamedLocationClient_GetIP(t *testing.T, c NamedLocationClientTest, id string) (ipNamedLocation *models.IPNamedLocation) {
+	ipNamedLocation, status, err := c.client.GetIP(c.connection.Context, id)
+	if err != nil {
+		t.Fatalf("IPNamedLocationClient.Get(): %v", err)
+	}
+	if status < 200 || status >= 300 {
+		t.Fatalf("IPNamedLocationClient.Get(): invalid status: %d", status)
+	}
+	if ipNamedLocation == nil {
+		t.Fatal("IPNamedLocationClient.Get(): ipNamedLocation was nil")
+	}
+	return
+}
+
+func testNamedLocationClient_GetCountry(t *testing.T, c NamedLocationClientTest, id string) (countryNamedLocation *models.CountryNamedLocation) {
+	countryNamedLocation, status, err := c.client.GetCountry(c.connection.Context, id)
+	if err != nil {
+		t.Fatalf("NamedLocationClient.GetCountry(): %v", err)
+	}
+	if status < 200 || status >= 300 {
+		t.Fatalf("NamedLocationClient.GetCountry(): invalid status: %d", status)
+	}
+	if countryNamedLocation == nil {
+		t.Fatal("NamedLocationClient.GetCountry(): countryNamedLocation was nil")
+	}
+	return
+}
+
+func testNamedLocationClient_UpdateIP(t *testing.T, c NamedLocationClientTest, ipnl models.IPNamedLocation) {
+	status, err := c.client.UpdateIP(c.connection.Context, ipnl)
+	if err != nil {
+		t.Fatalf("NamedLocationClient.UpdateIP(): %v", err)
+	}
+	if status < 200 || status >= 300 {
+		t.Fatalf("NamedLocationClient.UpdateIP(): invalid status: %d", status)
+	}
+}
+
+func testNamedLocationClient_UpdateCountry(t *testing.T, c NamedLocationClientTest, cnl models.CountryNamedLocation) {
+	status, err := c.client.UpdateCountry(c.connection.Context, cnl)
+	if err != nil {
+		t.Fatalf("NamedLocationClient.UpdateCountry(): %v", err)
+	}
+	if status < 200 || status >= 300 {
+		t.Fatalf("NamedLocationClient.UpdateCountry(): invalid status: %d", status)
+	}
+}
+
+func testNamedLocationClient_List(t *testing.T, c NamedLocationClientTest, f string) (namedLocations *[]models.NamedLocation) {
+	namedLocations, _, err := c.client.List(c.connection.Context, f)
+	if err != nil {
+		t.Fatalf("NamedLocationClient.List(): %v", err)
+	}
+	if namedLocations == nil {
+		t.Fatal("NamedLocationClient.List(): ipNamedLocations was nil")
+	}
+	return
+}
+
+func testNamedLocationClient_Delete(t *testing.T, c NamedLocationClientTest, id string) {
+	status, err := c.client.Delete(c.connection.Context, id)
+	if err != nil {
+		t.Fatalf("NamedLocationClient.Delete(): %v", err)
+	}
+	if status < 200 || status >= 300 {
+		t.Fatalf("NamedLocationClient.Delete(): invalid status: %d", status)
+	}
+}
