1922ca5b307af7ed66698d7a77ff32c654d830f7
alexwilcox9 <38583982+alexwilcox9@users.noreply.github.com>
2021-04-12 13:08:41 +0100

    Minor formatting improvements
    Co-authored-by: Tom Bamford <tom@bamford.io>

--- msgraph/namedlocation_test.go
+++ msgraph/namedlocation_test.go
@@ -27,7 +27,8 @@ func TestNamedLocationClient(t *testing.T) {
 
 	newIPNamedLocation := msgraph.IPNamedLocation{
 		BaseNamedLocation: &msgraph.BaseNamedLocation{
-			DisplayName: utils.StringPtr("Test IP Named Location")},
+			DisplayName: utils.StringPtr("Test IP Named Location")
+		},
 		IPRanges: &[]msgraph.IPNamedLocationIPRange{
 			{
 				CIDRAddress: utils.StringPtr("8.8.8.8/32"),
@@ -41,7 +42,8 @@ func TestNamedLocationClient(t *testing.T) {
 
 	newCountryNamedLocation := msgraph.CountryNamedLocation{
 		BaseNamedLocation: &msgraph.BaseNamedLocation{
-			DisplayName: utils.StringPtr("Test Country Named Location")},
+			DisplayName: utils.StringPtr("Test Country Named Location")
+		},
 		CountriesAndRegions: &[]string{"US", "GB"},
 	}
 
