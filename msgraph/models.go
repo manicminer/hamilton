144f73ce2393e54f9df5df5ddd64f7f4d5d987f8
Michal Jirman <jirman.michal@gmail.com>
2021-09-13 09:41:52 +0100

    Add support for employee org data (#99)
    * add support for employee org data
    
    * code refactor

--- msgraph/models.go
+++ msgraph/models.go
@@ -1269,6 +1269,7 @@ type User struct {
 	DisplayName                     *string                  `json:"displayName,omitempty"`
 	EmployeeHireDate                *time.Time               `json:"employeeHireDate,omitempty"`
 	EmployeeId                      *StringNullWhenEmpty     `json:"employeeId,omitempty"`
+	EmployeeOrgData                 *EmployeeOrgData         `json:"employeeOrgData,omitempty"`
 	EmployeeType                    *string                  `json:"employeeType,omitempty"`
 	ExternalUserState               *string                  `json:"externalUserState,omitempty"`
 	FaxNumber                       *StringNullWhenEmpty     `json:"faxNumber,omitempty"`
@@ -1430,4 +1431,9 @@ type WindowsHelloForBusinessAuthenticationMethod struct {
 	DisplayName     *string                          `json:"displayName,omitempty"`
 	ID              *string                          `json:"id,omitempty"`
 	KeyStrength     *AuthenticationMethodKeyStrength `json:"authenticationMethodKeyStrength,omitempty"`
+}
+
+type EmployeeOrgData struct {
+	CostCenter *string `json:"costCenter,omitempty"`
+	Division   *string `json:"division,omitempty"`
 }
