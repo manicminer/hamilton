96ff8d22d5c8ced87ff3e7e156878b768a9e1f58
kaovd <29356754+kaovd@users.noreply.github.com>
2022-01-17 12:22:58 +0000

    EntitlementManagement - Add Types, move catalogs and packages to 1.0 Stable (#133)
    * Add Types, move catalogs and packages to 1.0 Stable
    
    * Fmt, fix idempotency
    
    * Add tests and models to fix odata requirement in questions collection
    
    * Fix fmt, make compatible with beta
    
    * Add ReviewSettings tests, additional model fixes and valuetypes
    
    * Fix value types - Force workflow rerun
    
    * Readd
    
    * Rerun tests
    
    * rerun tests
    
    Co-authored-by: kaovd <kaovd@protonmail.com>
    Co-authored-by: kaovd <kaovd@kaovd>

--- msgraph/accesspackageassignmentpolicy_test.go
+++ msgraph/accesspackageassignmentpolicy_test.go
@@ -3,6 +3,7 @@ package msgraph_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/manicminer/hamilton/internal/test"
 	"github.com/manicminer/hamilton/internal/utils"
@@ -20,16 +21,48 @@ func TestAccessPackageAssignmentPolicyClient(t *testing.T) {
 	// Create AP
 	accessPackage := testAccessPackage_Create(t, c, accessPackageCatalog)
 
+	currentTimePlusDay := time.Now().AddDate(0, 0, 1)
+
 	// Create Assignment Policy
 	accessPackageAssignmentPolicy := testAccessPackageAssignmentPolicyClient_Create(t, c, msgraph.AccessPackageAssignmentPolicy{
 		AccessPackageId: accessPackage.ID,
-		DisplayName:     utils.StringPtr(fmt.Sprintf("Test-AP-Policy-Assignment-%s", c.RandomString)),
-		Description:     utils.StringPtr("Test AP Policy Assignment Description"),
+		AccessReviewSettings: &msgraph.AssignmentReviewSettings{
+			AccessReviewTimeoutBehavior:     msgraph.AccessReviewTimeoutBehaviorTypeRemoveAccess,
+			IsEnabled:                       utils.BoolPtr(true),
+			StartDateTime:                   &currentTimePlusDay,
+			DurationInDays:                  utils.Int32Ptr(5),
+			RecurrenceType:                  msgraph.AccessReviewRecurranceTypeMonthly,
+			ReviewerType:                    msgraph.AccessReviewReviewerTypeSelf,
+			IsAccessRecommendationEnabled:   utils.BoolPtr(true),
+			IsApprovalJustificationRequired: utils.BoolPtr(true),
+			// Reviewers: &[]msgraph.UserSet{
+			// 	{
+			// 		ODataType:    utils.StringPtr(odata.TypeRequestorManager),
+			// 		IsBackup:     utils.BoolPtr(false),
+			// 		ManagerLevel: utils.Int32Ptr(1),
+			// 	},
+			// 	{
+			// 		ODataType:    utils.StringPtr(odata.TypeSingleUser),
+			// 		IsBackup:     utils.BoolPtr(true),
+			// 		ID:           utils.StringPtr(""),
+			// 	},
+			// },
+		},
+		DisplayName: utils.StringPtr(fmt.Sprintf("Test-AP-Policy-Assignment-%s", c.RandomString)),
+		Description: utils.StringPtr("Test AP Policy Assignment Description"),
 		//AccessReviewSettings: utils.BoolPtr()
 		RequestorSettings: &msgraph.RequestorSettings{
+			//ScopeType:      msgraph.RequestorSettingsScopeTypeSpecificDirectorySubjects,
 			ScopeType:      msgraph.RequestorSettingsScopeTypeNoSubjects,
 			AcceptRequests: utils.BoolPtr(true),
-			//AllowedRequestors: &msgraph.UserSet{}
+			// AllowedRequestors: &[]msgraph.UserSet{
+			// 		{
+			// 			ODataType: utils.StringPtr(odata.TypeGroupMembers),
+			// 			IsBackup: utils.BoolPtr(false),
+			// 			ID: utils.StringPtr("xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"),
+			// 			Description: utils.StringPtr("Sample users group"),
+			// 		},
+			// },
 		},
 		RequestApprovalSettings: &msgraph.ApprovalSettings{
 			IsApprovalRequired:               utils.BoolPtr(false),
@@ -37,6 +70,68 @@ func TestAccessPackageAssignmentPolicyClient(t *testing.T) {
 			IsRequestorJustificationRequired: utils.BoolPtr(false),
 			ApprovalMode:                     msgraph.ApprovalModeNoApproval,
 			//ApprovalStages: &msgraph.ApprovalStages{},
+		},
+		Questions: &[]msgraph.AccessPackageQuestion{
+			{
+				ODataType:  utils.StringPtr(odata.TypeAccessPackageTextInputQuestion),
+				IsRequired: utils.BoolPtr(false),
+				Sequence:   utils.Int32Ptr(1),
+				Text: &msgraph.AccessPackageLocalizedContent{
+					DefaultText: utils.StringPtr("Test"),
+					LocalizedTexts: &[]msgraph.AccessPackageLocalizedTexts{
+						{
+							Text:         utils.StringPtr("abc"),
+							LanguageCode: utils.StringPtr("en"),
+						},
+					},
+				},
+			},
+			{
+				ODataType:  utils.StringPtr(odata.TypeAccessPackageMultipleChoiceQuestion),
+				IsRequired: utils.BoolPtr(false),
+				Sequence:   utils.Int32Ptr(2),
+				Text: &msgraph.AccessPackageLocalizedContent{
+					DefaultText: utils.StringPtr("Test"),
+					LocalizedTexts: &[]msgraph.AccessPackageLocalizedTexts{
+						{
+							Text:         utils.StringPtr("abc 2"),
+							LanguageCode: utils.StringPtr("gb"),
+						},
+					},
+				},
+				Choices: &[]msgraph.AccessPackageMultipleChoiceQuestions{
+					// Choice 1 containing a list of languages
+					{
+						ActualValue: utils.StringPtr("CHOICE1"),
+						DisplayValue: &msgraph.AccessPackageLocalizedContent{
+							DefaultText: utils.StringPtr("One"),
+							LocalizedTexts: &[]msgraph.AccessPackageLocalizedTexts{
+								{
+									Text:         utils.StringPtr("Choice 1"),
+									LanguageCode: utils.StringPtr("gb"),
+								},
+							},
+						},
+					},
+					// Choice 2 containing a list of languages, etc.
+					{
+						ActualValue: utils.StringPtr("CHOICE2"),
+						DisplayValue: &msgraph.AccessPackageLocalizedContent{
+							DefaultText: utils.StringPtr("Two"),
+							LocalizedTexts: &[]msgraph.AccessPackageLocalizedTexts{
+								{
+									Text:         utils.StringPtr("Choice 2"),
+									LanguageCode: utils.StringPtr("gb"),
+								},
+								{
+									Text:         utils.StringPtr("Zwei"),
+									LanguageCode: utils.StringPtr("de"),
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	})
 
@@ -128,11 +223,12 @@ func testAccessPackageAssignmentPolicyClient_Delete(t *testing.T, c *test.Test, 
 
 func testAccessPackage_Create(t *testing.T, c *test.Test, accessPackageCatalog *msgraph.AccessPackageCatalog) (accessPackage *msgraph.AccessPackage) {
 	accessPackage, _, err := c.AccessPackageClient.Create(c.Context, msgraph.AccessPackage{
-		DisplayName:         utils.StringPtr(fmt.Sprintf("test-accesspackage-%s", c.RandomString)),
-		CatalogId:           accessPackageCatalog.ID,
-		Description:         utils.StringPtr("Test Access Package"),
-		IsHidden:            utils.BoolPtr(false),
-		IsRoleScopesVisible: utils.BoolPtr(false),
+		DisplayName: utils.StringPtr(fmt.Sprintf("test-accesspackage-%s", c.RandomString)),
+		Catalog: &msgraph.AccessPackageCatalog{
+			ID: accessPackageCatalog.ID,
+		},
+		Description: utils.StringPtr("Test Access Package"),
+		IsHidden:    utils.BoolPtr(false),
 	})
 
 	if err != nil {
@@ -154,7 +250,7 @@ func testAccessPackageCatalog_Create(t *testing.T, c *test.Test) (accessPackageC
 	accessPackageCatalog, _, err := c.AccessPackageCatalogClient.Create(c.Context, msgraph.AccessPackageCatalog{
 		DisplayName:         utils.StringPtr(fmt.Sprintf("test-catalog-%s", c.RandomString)),
 		CatalogType:         msgraph.AccessPackageCatalogTypeUserManaged,
-		CatalogStatus:       msgraph.AccessPackageCatalogStatusPublished,
+		State:               msgraph.AccessPackageCatalogStatePublished,
 		Description:         utils.StringPtr("Test Access Catalog"),
 		IsExternallyVisible: utils.BoolPtr(false),
 	})
