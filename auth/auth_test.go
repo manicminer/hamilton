fe603929027ebd647cd595d3a5fc85f18ce7ec18
Tom Bamford <tom@bamford.io>
2022-03-10 19:07:48 +0000

    Remove autorest dependency; wrapper structs/functions have been moved to a new module

--- auth/auth_test.go
+++ auth/auth_test.go
@@ -5,8 +5,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/adal"
 	"golang.org/x/oauth2"
 
 	"github.com/manicminer/hamilton/auth"
@@ -159,43 +157,6 @@ func TestMsiAuthorizer(t *testing.T) {
 	auth, err := auth.NewMsiAuthorizer(ctx, env.MsGraph, msiEndpoint, clientId)
 	if err != nil {
 		t.Fatalf("NewMsiAuthorizer(): %v", err)
-	}
-	if auth == nil {
-		t.Fatal("auth is nil, expected Authorizer")
-	}
-
-	token, err := auth.Token()
-	if err != nil {
-		t.Fatalf("auth.Token(): %v", err)
-	}
-	if token == nil {
-		t.Fatal("token was nil")
-	}
-	if token.AccessToken == "" {
-		t.Fatal("token.AccessToken was empty")
-	}
-}
-
-func TestAutorestAuthorizerWrapper(t *testing.T) {
-	env, err := environments.EnvironmentFromString(environment)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// adal.ServicePrincipalToken.refreshInternal() doesn't support v2 tokens
-	oauthConfig, err := adal.NewOAuthConfigWithAPIVersion(string(env.AzureADEndpoint), tenantId, utils.StringPtr("1.0"))
-	if err != nil {
-		t.Fatalf("adal.NewOAuthConfig(): %v", err)
-	}
-
-	spt, err := adal.NewServicePrincipalToken(*oauthConfig, clientId, clientSecret, string(env.MsGraph.Endpoint))
-	if err != nil {
-		t.Fatalf("adal.NewServicePrincipalToken(): %v", err)
-	}
-
-	auth, err := auth.NewAutorestAuthorizerWrapper(autorest.NewBearerAuthorizer(spt))
-	if err != nil {
-		t.Fatalf("NewAutorestAuthorizerWrapper(): %v", err)
 	}
 	if auth == nil {
 		t.Fatal("auth is nil, expected Authorizer")
--- go.mod
+++ go.mod
@@ -3,15 +3,12 @@ module github.com/manicminer/hamilton
 go 1.16
 
 require (
-	github.com/Azure/go-autorest/autorest v0.11.21
-	github.com/Azure/go-autorest/autorest/adal v0.9.14
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.0
 	github.com/hashicorp/go-uuid v1.0.2
 	github.com/hashicorp/go-version v1.3.0
-	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
-	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3
 	golang.org/x/oauth2 v0.0.0-20210514164344-f6687ab2804c
 	google.golang.org/appengine v1.6.7 // indirect
 )
